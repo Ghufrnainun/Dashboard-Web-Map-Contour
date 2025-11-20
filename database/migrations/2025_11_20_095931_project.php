9956e732cc75b278b2108e7b4faf2ac8c8a35852
Ghufron Ainun Najib <ghufrnainunajib@gmail.com>
2025-11-20 16:59:50 +0700

    add database

---
+++ database/migrations/2025_11_20_095931_project.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // 1. Tabel Projects (Folder Survey)
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nama Lokasi/Project
+            $table->string('surveyor_name'); // Nama Mahasiswa
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+
+        // 2. Tabel Measurements (Data Sensor)
+        Schema::create('measurements', function (Blueprint $table) {
+            $table->id();
+            // Relasi ke Project (Kalau project dihapus, data hilang)
+            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
+            
+            // Koordinat (Presisi tinggi: 10 digit, 7 desimal)
+            $table->decimal('latitude', 10, 7); 
+            $table->decimal('longitude', 10, 7);
+            
+            // Data Sensor
+            $table->float('altitude'); // Ketinggian (meter)
+            $table->decimal('pressure', 8, 2)->nullable(); // Tekanan (hPa)
+            
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('measurements');
+        Schema::dropIfExists('projects');
+    }
+};
